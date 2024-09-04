42044ec773f49591954b7cd00b14e64cd88452f6
Aman Purohit <purohiam@sheridancollege.ca>
2024-09-03 23:22:36 -0400

    footer added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,29 +21,35 @@ const Header: React.FC = () => {
     }, []);
 
     return (
-        <div className="flex-col justify-center align-items-center text-center">
+        <div className="flex flex-col justify-center items-center text-center w-[100vw]">
             <div>
                 <h1
                     style={{ fontFamily: "Cinzel", color: "#E4E2DD" }}
-                    className="text-6xl font-light w-[60vw]  text-[#E4E2DD] md:text-xl pb-2 bg-slate-100"
+                    className="text-6xl font-light text-[#E4E2DD] md:text-xl pb-2"
                     onMouseEnter={playAudio}
                     onClick={playAudio}
-
                 >
                     Sheridan Swiftie Club
                 </h1>
-                <div style={{ fontFamily: "Cinzel" }} className=' flex-col justify-center align-items-center text-[#E4E2DD] text-sm gap-2  uppercase  p-10'>
-                    <p>The official website for Sheridan Swiftie Club</p>
-                    <p>We’re your go-to at Sheridan for all things Taylor Swift! If you’d like to learn more about the club, head to Clubs Corner and search for us!</p>
-                </div>
             </div>
-
-            <div >
+            <div
+                style={{ fontFamily: "Cinzel" }}
+                className="flex flex-col justify-center items-center text-[#E4E2DD] text-lg gap-2 uppercase h-[40vh] p-8 text-wrap w-[90vw]  md:text-lg md:w-[40vw] lg:w-[30vw] bg-opacity-50 rounded-md"
+            >
+                <p>The official website for Sheridan Swiftie Club</p>
+                <p>We’re your go-to at Sheridan for all things Taylor Swift! If you’d like to learn more about the club, head to Clubs Corner and search for us!</p>
+            </div>
+            <div className='flex-col text-center w-[100vw]'>
                 <LinkComponent />
             </div>
-
+            <footer >
+                <div style={{ fontFamily: "Cinzel" }} className='flex flex-col justify-center w-[90vw] p-16 items-center text-center text-[#E4E2DD]'>
+                    <p className='text-sm p-2'>Disclaimer: We're are not affiliated with Taylor Nation</p>
+                    <p className='text-sm p-1'>Designed and developed by Sheridan Swiftie Club</p>
+                    <p className='text-sm p-1'>© 2024 Sheridan Swiftie Club</p>
+                </div>
+            </footer>
         </div>
-
     );
 };
 
--- src/components/LinkComponent/LinkComponent.tsx
+++ src/components/LinkComponent/LinkComponent.tsx
@@ -3,7 +3,7 @@ import LinkButton from './LinkButton'
 import EmailButton from './EmailButton'
 const LinkComponent = () => {
     return (
-        <div style={{ fontFamily: "Cinzel" }} className='flex-col-reverse text-center text-[#E4E2DD] font-light text-2xl gap-2 w-[60vw] h-[20vh]'>
+        <div style={{ fontFamily: "Cinzel" }} className='flex-col-reverse text-center text-[#E4E2DD] font-light text-2xl gap-2 '>
 
             <LinkButton LinkLabel="Instagram" LinkUrl="https://www.instagram.com/sheridanswiftieclub/" />
             <LinkButton LinkLabel="Clubs Corner" LinkUrl="https://sheridancollege.campuslabs.ca/engage/organization/sheridanswiftieclub" />
