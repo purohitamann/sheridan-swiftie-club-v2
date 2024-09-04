817d13f038f13e4f542b224dee36b327c4b3f71d
Aman Purohit <purohiam@sheridancollege.ca>
2024-09-03 23:36:29 -0400

    eslint issue fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import LinkComponent from './LinkComponent/LinkComponent';
 
 const Header: React.FC = () => {
-    let audio: HTMLAudioElement | null = null;
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
     const playAudio = () => {
-        if (audio) {
-            audio.volume = 0.5;
-            audio.play();
+        if (audioRef.current) {
+            audioRef.current.volume = 0.5;
+            audioRef.current.play();
         }
     };
 
     useEffect(() => {
-
         if (typeof window !== 'undefined') {
-            audio = new Audio('/so-high-school-intro.mp3');
-
+            audioRef.current = new Audio('/so-high-school-intro.mp3');
             playAudio();
         }
     }, []);
 
     return (
-        <div className="flex flex-col justify-center items-center text-center w-[100vw]">
+        <div className="flex flex-col justify-center items-center text-center w-[100vw] min-h-screen pt-10 ">
             <div>
                 <h1
                     style={{ fontFamily: "Cinzel", color: "#E4E2DD" }}
-                    className="text-6xl font-light text-[#E4E2DD] md:text-xl pb-2"
+                    className="text-6xl font-light md:text-xl pb-2"
                     onMouseEnter={playAudio}
                     onClick={playAudio}
                 >
@@ -33,20 +31,20 @@ const Header: React.FC = () => {
                 </h1>
             </div>
             <div
-                style={{ fontFamily: "Cinzel" }}
-                className="flex flex-col justify-center items-center text-[#E4E2DD] text-lg gap-2 uppercase h-[40vh] p-8 text-wrap w-[90vw]  md:text-lg md:w-[40vw] lg:w-[30vw] bg-opacity-50 rounded-md"
+                style={{ fontFamily: "Cinzel", backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+                className="flex flex-col justify-center items-center text-[#E4E2DD] text-lg gap-2 uppercase h-[40vh] mt-8  p-10 text-wrap w-[90vw] md:w-[40vw] lg:w-[30vw] rounded-md"
             >
                 <p>The official website for Sheridan Swiftie Club</p>
                 <p>We’re your go-to at Sheridan for all things Taylor Swift! If you’d like to learn more about the club, head to Clubs Corner and search for us!</p>
             </div>
-            <div className='flex-col text-center w-[100vw]'>
+            <div className="flex-col text-center w-[100vw] pt-10">
                 <LinkComponent />
             </div>
-            <footer >
-                <div style={{ fontFamily: "Cinzel" }} className='flex flex-col justify-center w-[90vw] p-16 items-center text-center text-[#E4E2DD]'>
-                    <p className='text-sm p-2'>Disclaimer: We're are not affiliated with Taylor Nation</p>
-                    <p className='text-sm p-1'>Designed and developed by Sheridan Swiftie Club</p>
-                    <p className='text-sm p-1'>© 2024 Sheridan Swiftie Club</p>
+            <footer className="mt-auto">
+                <div style={{ fontFamily: "Cinzel" }} className="flex flex-col justify-center items-center w-[90vw] p-8 text-center text-[#E4E2DD]">
+                    <p className="text-sm p-2">Disclaimer: We&#39;re not affiliated with Taylor Nation</p>
+                    <p className="text-sm p-1">Designed and developed by Sheridan Swiftie Club</p>
+                    <p className="text-sm p-1">© 2024 Sheridan Swiftie Club</p>
                 </div>
             </footer>
         </div>
